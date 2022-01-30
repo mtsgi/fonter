5271c0cf1bc01c0ad029b49b9bd7d6acd5895128
mtsgi <oq@live.jp>
2022-01-31 01:17:51 +0900

    Add prettier and sass

--- src/components/TheFonter.vue
+++ src/components/TheFonter.vue
@@ -1,52 +1,24 @@
 <script setup lang="ts">
-import { ref } from 'vue'
+import { ref, computed } from "vue";
 
-defineProps<{ msg: string }>()
-
-const count = ref(0)
+const text = ref("");
+const length = computed(() => {
+  return text.value.length
+})
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
-
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+  <textarea
+    type="text"
+    v-model="text"
+    placeholder="Type something here"
+  />
+  <div>{{ length }}</div>
 </template>
 
-<style scoped>
-a {
-  color: #42b983;
-}
-
-label {
-  margin: 0 0.5em;
-  font-weight: bold;
-}
-
-code {
-  background-color: #eee;
-  padding: 2px 4px;
-  border-radius: 4px;
-  color: #304455;
+<style lang="scss" scoped>
+textarea {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  padding: 8px;
 }
 </style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <h1>Fonter</h1>
+  </header>
+</template>
+
+<style lang="scss" scoped></style>
